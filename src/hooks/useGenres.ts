e092453e3aa3b271006bd7d2d79246c42156818c
Nahom Tigistu <nahomtigistu777@gmail.com>
2025-07-10 17:58:55 +0300

    Fetch the genres

---
+++ src/hooks/useGenres.ts
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react';
+import apiClient from '../services/api-client';
+import { CanceledError } from 'axios';
+
+interface Genre {
+  id: number;
+  name: string;
+}
+
+interface FetchGenresResponse {
+  count: number;
+  results: Genre[];
+}
+
+const useGenres = () => {
+  const [genres, setGenres] = useState<Genre[]>([]); // State variable for our games
+  const [error, setError] = useState(''); // State variable for our error messages
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    // For handling cancellation
+    // Our cancellation function is executed when a user goes away from that page OR the component is unmounted
+    const controller = new AbortController();
+    //  AbortController - is built in class in modern browsers that allows us to
+    //    cancel or abort async operations like fetch request, DOM manipulation or any operation that may take long time to complete
+
+    ///////////////////////////////////////////////////
+    // The use cancelling requests is to avoid background running of
+    // the fetch request when the user is not in that page.
+    /////////////////////////////////////////////////
+
+    setLoading(true);
+    apiClient
+      .get<FetchGenresResponse>('/genres', { signal: controller.signal }) // adding a configuration object
+      .then(res => {
+        setGenres(res.data.results);
+      })
+      .catch(err => {
+        // Since on development mode react renders twice, when it unmounts the first render our error would be "Canceled Error", and when it mounts the 2nd component the error message is displayed at the top, .... so we are protecting this error to be shown
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      })
+      .finally(() => setLoading(false));
+
+    // This cleanup function is optionally returned from useEffect (this function), it may not always returned
+    // that means it is only returned when the component is unmounted
+    return () => controller.abort(); // Cleanup function
+  }, []);
+
+  return { genres, error, isLoading };
+};
+
+export default useGenres;
