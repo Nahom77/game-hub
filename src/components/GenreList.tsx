0dbc85b62c058487331c46a770f654a561665106
Nahom Tigistu <nahomtigistu777@gmail.com>
2025-07-16 03:41:20 +0300

    Clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,5 +1,6 @@
 import {
   Button,
+  Heading,
   HStack,
   Image,
   List,
@@ -23,28 +24,36 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   if (isLoading) return <Spinner />;
 
   return (
-    <List>
-      {/* {isLoading && <Spinner />} */}
-      {data.map(genre => (
-        <ListItem key={genre.id} paddingY='5px'>
-          <HStack>
-            <Image
-              boxSize='32px'
-              borderRadius={8}
-              src={getCroppedImageUrl(genre.image_background)}
-            />
-            <Button
-              onClick={() => onSelectGenre(genre)}
-              fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
-              fontSize='lg'
-              variant='link'
-            >
-              {genre.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading marginBottom={3} fontSize='2xl'>
+        Genres
+      </Heading>
+      <List>
+        {/* {isLoading && <Spinner />} */}
+        {data.map(genre => (
+          <ListItem key={genre.id} paddingY='5px'>
+            <HStack>
+              <Image
+                src={getCroppedImageUrl(genre.image_background)}
+                boxSize='32px'
+                borderRadius={8}
+                objectFit='cover'
+              />
+              <Button
+                onClick={() => onSelectGenre(genre)}
+                whiteSpace='normal'
+                textAlign='left'
+                fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
+                fontSize='lg'
+                variant='link'
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
